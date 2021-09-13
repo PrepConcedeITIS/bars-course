c569c6251ec42c7853f4673a760c8988e78b4ff2
Tagmir Gilyazov <t1g2r3mr@gmail.com>
2021-09-13 09:26:20 +0300

    Initial commit

---
+++ TestService/Controllers/DocumentStatusController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentStatusController : ControllerBase
+    {
+        private readonly ILogger<DocumentStatusController> _logger;
+
+        public DocumentStatusController(ILogger<DocumentStatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(DocumentStatusInfo), StatusCodes.Status200OK)]
+        public IActionResult Get(long id)
+        {
+            var mockObject = new DocumentStatusInfo(id, DocumentStatus.New);
+            return Ok(mockObject);
+        }
+    }
+
+    public class DocumentStatusInfo
+    {
+        public DocumentStatusInfo(long id, DocumentStatus status)
+        {
+            Id = id;
+            Status = status;
+        }
+        public long Id { get; }
+        public DocumentStatus Status { get; }
+        public string Name => Status.ToString();
+    }
+    
+    public enum DocumentStatus
+    {
+        New
+    }
+}
